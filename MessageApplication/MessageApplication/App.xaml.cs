f5323479221448bb397b966494f83a7f2951ca74
haleonas <altes@live.se>
2020-10-29 15:55:01 +0100

    initial setup

---
+++ MessageApplication/MessageApplication/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using MessageApplication.Views;
+using Microsoft.WindowsAzure.MobileServices;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+
+namespace MessageApplication
+{
+    public partial class App : Application
+    {
+        
+        public static MobileServiceClient _Client = new MobileServiceClient("https://messageapplication.azurewebsites.net");   
+        
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ MessageApplication/MessageApplication/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace MessageApplication.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
